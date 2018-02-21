aa6b8028efcfed1aa26ceacd356cc4745f8ed554
UjjawalX <besrau@yahoo.com>
2018-02-21 19:02:47 +0530

    first commit

--- src/main/java/com/misys/hackathon/chatbot/controller/MessagesController.java
+++ src/main/java/com/misys/hackathon/chatbot/controller/MessagesController.java
@@ -10,8 +10,15 @@ import com.misys.hackathon.chatbot.model.authDCRs.AuthenticationDCPassRs;
 import com.misys.hackathon.chatbot.nlpProcessor.NlpProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @RestController
@@ -21,6 +28,8 @@ public class MessagesController {
     RestTemplate restTemplate = new RestTemplate();
     Logger logger = LoggerFactory.getLogger("MessagesController.class");
     String userLocator = "";
+    String sessionId;
+    String dcUrl = "http://fr1csldcdev0013.misys.global.ad:9086/foundation-guiwar/security/pegasus_security_check";
     @RequestMapping("/messages")
     public Messages getMessages(){
         RestTemplate restTemplate = new RestTemplate();
@@ -41,6 +50,7 @@ public class MessagesController {
     public AuthenticationMessage postAuthenticateUsr(@RequestBody AuthenticationRq authenticationRq){
         AuthenticationDCIdRs authenticationDCIdRs = null;
         AuthenticationDCIdRq authenticationDCIdRq = new AuthenticationDCIdRq();
+        HttpEntity<AuthenticationDCIdRs> response = null;
         if(authenticationRq!=null&&!authenticationRq.getUserid().equals("")){
             //set Data
             UserData userData = new UserData();
@@ -55,27 +65,36 @@ public class MessagesController {
             header.setUserInputCaptchaData(captchaData);
             authenticationDCIdRq.setHeader(header);
             //service call
-            authenticationDCIdRs = restTemplate.postForObject("http://blrcslfbpub0006.misys.global.ad:9080/foundation-guiwar/security/pegasus_security_check",authenticationDCIdRq, AuthenticationDCIdRs.class);
+            //authenticationDCIdRs = restTemplate.postForObject("http://blrcslfbpub0006.misys.global.ad:9080/foundation-guiwar/security/pegasus_security_check",authenticationDCIdRq, AuthenticationDCIdRs.class);
+//            String url = "http://blrcslfbpub0006.misys.global.ad:9080/foundation-guiwar/security/pegasus_security_check";
+            // String url = "http://fr1csldcdev0013.misys.global.ad:9086/foundation-guiwar/security/pegasus_security_check";
+            HttpEntity<AuthenticationDCIdRq> request = new HttpEntity<>(authenticationDCIdRq);
+            response = restTemplate.exchange(dcUrl, HttpMethod.POST,request,AuthenticationDCIdRs.class);
+            List<String> cookie = response.getHeaders().get("Set-Cookie");
+            sessionId = Arrays.asList(cookie.get(0).split(";")).get(0);
+            logger.info("Set-Cookie: "+cookie.toString());
+            logger.info("Session Id: "+sessionId);
+
         }
         AuthenticationMessage authenticationMessage = new AuthenticationMessage();
-        if(authenticationDCIdRs != null && authenticationDCIdRs.isSuccess()){
-            authenticationMessage.setStatus("success");
-            authenticationMessage.setMessage("DC user authorized");
-            logger.info("DC user authorized");
+
+        if(response != null && response.getBody()!= null && response.getBody().isSuccess()){
+            logger.info("DC user name authorized");
+            authenticationMessage = postAuthenticatePass(authenticationRq);
         }
         else{
-            authenticationMessage.setMessage("authorization failure");
+            authenticationMessage.setMessage("user name authorization failure");
             authenticationMessage.setStatus("failure");
         }
 
         return authenticationMessage;
     }
 
-    @RequestMapping(value="/security/loginpass",method = RequestMethod.POST )
-    public AuthenticationMessage postAuthenticatePass(@RequestBody AuthenticationRq authenticationRq){
+    public AuthenticationMessage postAuthenticatePass(AuthenticationRq authenticationRq){
         AuthenticationDCPassRq authenticationDCPassRq = new AuthenticationDCPassRq();
         AuthenticationMessage authenticationMessage = new AuthenticationMessage();
-        AuthenticationDCPassRs authenticationDCPassRs = null;
+        HttpEntity<AuthenticationDCPassRq> request = null;
+        HttpEntity<AuthenticationDCPassRs> response = null;
         if(authenticationRq!=null&&!authenticationRq.getPassword().equals("")){
             //set header
             Header1 header1 = new Header1();
@@ -87,17 +106,24 @@ public class MessagesController {
             data.setData(userData1);
             authenticationDCPassRq.setData(data);
             //service call
-            authenticationDCPassRs = restTemplate.postForObject("http://blrcslfbpub0006.misys.global.ad:9080/foundation-guiwar/security/pegasus_security_check",authenticationDCPassRq,AuthenticationDCPassRs.class);
+            //authenticationDCPassRs = restTemplate.postForObject("http://blrcslfbpub0006.misys.global.ad:9080/foundation-guiwar/security/pegasus_security_check",authenticationDCPassRq,AuthenticationDCPassRs.class);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("cookie",sessionId);
+            request = new HttpEntity(authenticationDCPassRq,httpHeaders);
+//            String url = "http://blrcslfbpub0006.misys.global.ad:9080/foundation-guiwar/security/pegasus_security_check";
+            //String url = "http://fr1csldcdev0013.misys.global.ad:9086/foundation-guiwar/security/pegasus_security_check";
+            response = restTemplate.exchange(dcUrl, HttpMethod.POST,request,AuthenticationDCPassRs.class);
+
         }
-        if(authenticationDCPassRs.isSuccess()){
+        if(response!=null && response.getBody()!=null && response.getBody().isSuccess()){
             logger.info("DC password authentication succeded");
             authenticationMessage.setStatus("success");
-            authenticationMessage.setMessage("DC password authentication succeded");
+            authenticationMessage.setMessage("DC authentication succeded");
         }
         else {
             logger.info("failed : DC password authentication failed");
             authenticationMessage.setStatus("fail");
-            authenticationMessage.setMessage("failed : DC password authentication failed");
+            authenticationMessage.setMessage("failed : DC authentication failed");
         }
         return authenticationMessage;
     }
